d7cb30b788867a5dd7098d92b20cee1684004430
Chun Wai <wonderkidcw@yahoo.com.sg>
2026-07-08 22:38:15 +0800

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GoogleAnalytics from "./components/GoogleAnalytics";
 import Clarity from "./components/Clarity";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <GoogleAnalytics />
         <Clarity />
+        <Analytics />
         {children}
       </body>
     </html>
